56eb44a73d76702935aa8211708a99eb9cab69f7
Nathan <nathan@bluemesaengineering.com>
2020-06-10 00:15:29 -0400

    feat:Added validates_with to stock and datapoint classes

--- app/models/datapoint.rb
+++ app/models/datapoint.rb
@@ -1,3 +1,18 @@
+require "#{Rails.root}/app/models/stock"
+
+class DatapointValidator < ActiveModel::Validator
+  puts 'Entering DatapointValidator'
+  def validate(record)
+    stocks = Stock.select(:symbol)
+    stocks.each do |item|
+      if record.symbol === item.symbol
+        # puts 'datapoint matched with stock' + record.symbol.to_s 
+      end      
+    end
+  end
+end
+
 class Datapoint < ApplicationRecord
+  validates_with DatapointValidator
   belongs_to :stock
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,17 @@
-class Stock < ApplicationRecord
-  validates_with StockValidator
-  has_many :datapoints
+class StockValidator < ActiveModel::Validator
+  puts 'Entering StockValidator'
+  def validate(record)
+    if Stock.find_by(symbol: record.symbol).nil?
+      puts 'Stock added: ' + record.symbol.to_s
+    else
+      record.errors.add :base, 'Pre-existing Symbol Error for ' + record.symbol.to_s
+      puts record.errors.full_messages
+    end
+  end
 end
 
-class StockValidator < ActiveModel::Validator
-  validates :symbol, uniqueness: true, length: { minimum: 3, maximum: 4 }
+class Stock < ApplicationRecord
+  # validates :symbol, uniqueness: true, length: { minimum: 3, maximum: 4 }
+  validates_with StockValidator
+  has_many :datapoints
 end
